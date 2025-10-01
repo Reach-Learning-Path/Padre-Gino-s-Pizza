03967ac49a36e8aed28ca835b5a9f098bac41bf7
donnie <cs_neeko@yahoo.com>
2025-10-01 22:55:25 +0300

    Fix typos and improve formatting in HTML and React components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 const Pizza = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("p", {}, props.descripton)
-    ])
-}
-
-
-
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div", {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {
-                name: "Americano Pizza",
-                descripton: "French Fries and hot dogs wtf Italy ??!",
-            }),
-            React.createElement(Pizza, {
-                name: "Baked Patato Pizza",
-                descripton: "Baked Patato with sour cream and chives",
-            }),
-            React.createElement(Pizza, {
-                name: "Hawaiian Pizza",
-                descripton: "Pineaplle and Ham... okay",
-            }),
-            React.createElement(Pizza, {
-                name: "Veggie Pizza",
-                descripton: "Tomato, peppers, onions, mushrooms, olives",
-            }),
-            React.createElement(Pizza, {
-                name: "Dope Pizza",
-                descripton: "The dope pizza",
-            }),
-
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "French Fries and hot dogs wtf Italy ??!",
+    }),
+    React.createElement(Pizza, {
+      name: "Baked Potato Pizza",
+      description: "Baked Potato with sour cream and chives",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian Pizza",
+      description: "Pineapple and Ham... okay",
+    }),
+    React.createElement(Pizza, {
+      name: "Veggie Pizza",
+      description: "Tomato, peppers, onions, mushrooms, olives",
+    }),
+    React.createElement(Pizza, {
+      name: "Dope Pizza",
+      description: "The dope pizza",
+    }),
+  ]);
 };
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
